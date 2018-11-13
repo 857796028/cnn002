c7320cd301a28922c949ff7d3982f2f17beaad18
857796028 <857796028@qq.com>
2018-11-13 10:47:47 +0800

    cnn002第1047次提交

---
+++ webapi_springboottest002/src/main/java/com/example/springboot/WebapiSpringboottest002Application.java
@@ -0,0 +1,12 @@
+package com.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebapiSpringboottest002Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebapiSpringboottest002Application.class, args);
+	}
+}
